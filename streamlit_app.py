2a9e2ea8ec503efef7ac03534f87e47cf2051270
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2023-01-10 14:46:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,9 +12,12 @@ if ready:
       "What is a package manager?",
       ('A person who delivers Amazon shipments', 'A tool that automates the process of installing, upgrading, configuring, and removing dependencies', 'A method to keep your development environment separate from other projects on the same machine', 'A tool that automatically removes dependencies if they have been deprecated'))
 
-    if question_1 == 'A tool that automates the process of installing, upgrading, configuring, and removing dependencies':
-      st.success("That's right! Great job.", icon="✅")
-    elif question_1 == 'A person who delivers Amazon shipments' or question_1 == 'A method to keep your development environment separate from other projects on the same machine' or question_1 == 'A tool that automatically removes dependencies if they have been deprecated':
-      st.error('Uh oh. Wrong answer. Try again!', icon="🚨")
+    submit = st.button("Submit")
+    if submit:
+      if question_1 == 'A tool that automates the process of installing, upgrading, configuring, and removing dependencies':
+        st.success("That's right! Great job.", icon="✅")
+      else:
+        st.error('Uh oh. Wrong answer. Try again!', icon="🚨")
+    
 
   
