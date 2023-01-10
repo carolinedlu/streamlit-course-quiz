bf2365480202087c48af3136be24e17abd80b0fa
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2023-01-10 14:48:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,16 +9,17 @@ with placeholder.container():
 
 if ready:
   with placeholder.container():
-    question_1 = st.radio(
-      "What is a package manager?",
-      ('A person who delivers Amazon shipments', 'A tool that automates the process of installing, upgrading, configuring, and removing dependencies', 'A method to keep your development environment separate from other projects on the same machine', 'A tool that automatically removes dependencies if they have been deprecated'))
+    with st.form("question_1"):
+      question_1 = st.radio(
+        "What is a package manager?",
+        ('A person who delivers Amazon shipments', 'A tool that automates the process of installing, upgrading, configuring, and removing dependencies', 'A method to keep your development environment separate from other projects on the same machine', 'A tool that automatically removes dependencies if they have been deprecated'))
 
-    submit = st.button("Submit")
-    if submit:
-      if question_1 == 'A tool that automates the process of installing, upgrading, configuring, and removing dependencies':
-        st.success("That's right! Great job.", icon="✅")
-      else:
-        st.error('Uh oh. Wrong answer. Try again!', icon="🚨")
+      submit = st.form_submit_button("Submit")
+      if submit:
+        if question_1 == 'A tool that automates the process of installing, upgrading, configuring, and removing dependencies':
+          st.success("That's right! Great job.", icon="✅")
+        else:
+          st.error('Uh oh. Wrong answer. Try again!', icon="🚨")
     
 
   
