a707daa459a2a5d7a9a5523a950c36a6c900c5b3
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2023-01-10 14:13:00 -0500

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+placeholder = st.empty
+
+st.write('Are you ready to answer a few questions about installing Streamlit?')
+ready = placeholder.button('I'm ready!')
+
+if ready:
+  st.subheader('What is a package manager?')
