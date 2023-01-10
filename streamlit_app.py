03f138a14cbde568213d4886e4f3665751e625e4
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2023-01-10 14:35:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,17 @@ import streamlit as st
 
 placeholder = st.empty
 
-st.write('Are you ready to answer a few questions about installing Streamlit?')
+placeholder.write('Are you ready to answer a few questions about installing Streamlit?')
 ready = st.button("I'm ready!")
 
 if ready:
-  st.subheader('What is a package manager?')
+  question_1 = st.radio(
+    "What is a package manager?",
+    ('A person who delivers Amazon shipments', 'A tool that automates the process of installing, upgrading, configuring, and removing dependencies', 'A method to keep your development environment separate from other projects on the same machine', 'A tool that automatically removes dependencies if they have been deprecated'))
+  
+  if question_1 == 'A tool that automates the process of installing, upgrading, configuring, and removing dependencies':
+    st.success("That's right! Great job.", icon="✅")
+  else:
+    st.error('Uh oh. Wrong answer. Try again!', icon="🚨")
+
+  
