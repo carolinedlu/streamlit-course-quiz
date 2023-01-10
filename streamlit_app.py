4e7a537916638d8326e4620a32640c21cb900d73
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2023-01-10 14:43:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit as st
 
 placeholder = st.empty()
-placeholder.write('Are you ready to answer a few questions about installing Streamlit?')
-ready = placeholder.button("I'm ready!")
+
+with placeholder.container():
+  st.write('Are you ready to answer a few questions about installing Streamlit?')
+  ready = st.button("I'm ready!")
 
 if ready:
   with placeholder.container():
@@ -12,7 +14,7 @@ if ready:
 
     if question_1 == 'A tool that automates the process of installing, upgrading, configuring, and removing dependencies':
       st.success("That's right! Great job.", icon="✅")
-    else:
+    else if question_1 == 'A person who delivers Amazon shipments' or question_1 == A method to keep your development environment separate from other projects on the same machine' or question_1 == 'A tool that automatically removes dependencies if they have been deprecated':
       st.error('Uh oh. Wrong answer. Try again!', icon="🚨")
 
   
